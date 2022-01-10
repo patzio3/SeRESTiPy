fdd32fca63cee2a791ac64cd4dd3cbd8fc72a666
Patrick Eschenbach <p_esch01@wwu.de>
2022-01-10 18:30:49 +0100

    fix: made SDK work with slurm on Lyra

--- src/client/DockerSDK.py
+++ src/client/DockerSDK.py
@@ -1,39 +1,58 @@
 import docker
 import os, sys
+import socket
+import codecs
+from flask import Flask, request
+from flask_restful import Api, Resource
+
+class SwarmLauncher():
+    def run(self):
+        ncpu = sys.argv[1] if len(sys.argv) >= 2 else "4"
+        memory = sys.argv[2] if len(sys.argv) >= 3 else "48000"
+        nContainer = int(sys.argv[3]) if len(sys.argv) >= 4 else 8
+        client = docker.from_env()
+        # client.login(username = os.getenv("DOCKERHUB_USERNAME"), password = os.getenv("DOCKERHUB_PASSWORD"))
+        for i in range(nContainer):
+                outpath = os.path.join("/WORK/p_esch01/scratch_calc/")
+        	#os.mkdir(outpath)
+        	#run with one cpu
+                mount_map = docker.types.Mount("/home/calc", outpath, type = "bind")
+                client.containers.run('pesch01/serestipy:v1.4_service', 
+        	                      cpuset_cpus= ncpu, 
+        	                      mem_limit = memory+"m", 
+        	                      mounts = [mount_map],
+        	                      ports = {'5000/tcp': 5000 + i + 1},
+        	                      detach = True)
+        all_containers = client.containers.list()
+        ip_addresses = []
+        for index, container in enumerate(all_containers):
+            #ip_addresses.append("http://" + container.attrs['NetworkSettings']['IPAddress'] + ":" +str(5000 + index + 1))
+            ip_addresses.append("http://" + socket.gethostbyname(socket.gethostname()) + ":" + str(5000 + index + 1))
+        
+        with open("/WORK/p_esch01/scratch_calc/ips_hosts", "a") as handle:
+            for i in ip_addresses:
+                handle.write(str(i)+"\n")
+       
 
 
-ncpu = sys.argv[1] if len(sys.argv) >= 2 else "4"
-memory = sys.argv[2] if len(sys.argv) >= 3 else "48000"
-nContainer = int(sys.argv[3]) if len(sys.argv >= 4) else 8
+app = Flask(__name__)
+api = Api(app)
+class SDKPrune(Resource):
+    @app.route("/api/", methods = ["POST"])
+    def prune():
+        client = docker.from_env()
+        for container in client.containers.list(): 
+            container.stop()
+        os.system("docker container prune -f")
+        shutdown_func = request.environ.get('werkzeug.server.shutdown')
+        if shutdown_func is None:
+            raise RuntimeError('Not running werkzeug')
+        shutdown_func()
 
 
-client = docker.from_env()
-# client.login(username = os.getenv("DOCKERHUB_USERNAME"), password = os.getenv("DOCKERHUB_PASSWORD"))
+api.add_resource(SDKPrune, "/api/")
 
-for i in range(nContainer):
-	outpath = os.path.join("/home/calc","container"+str(i))
-	os.mkdir(outpath)
-	#run with one cpu
-	mount_map = docker.types.Mount("/home/calc", outpath, type = "bind")
-	client.containers.run('pesch01/serestipy:v1.4_service', 
-	                      cpuset_cpus= ncpu, 
-	                      mem_limit = f"{memory}m", 
-	                      mounts = [mount_map],
-	                      #ports = {'5000/tcp': 5000},
-	                      detach = True)
-
-
-all_containers = client.containers.list()
-ip_addresses = []
-for container in all_containers:
-	ip_addresses.append(container.attrs['NetworkSettings']['IPAddress'])
-
-print(ip_addresses)
-
-#for container in all_containers:
-#        container.stop()
-#for container in all_containers:
-#        container.start()
-
-
-
+if __name__ == "__main__":
+    launcher = SwarmLauncher()
+    launcher.run()
+    app.run(host=socket.gethostbyname(socket.gethostname()), debug=False)
--- src/client/PrepareSLURM.py
+++ src/client/PrepareSLURM.py
@@ -1,37 +1,39 @@
 #!/usr/bin/env python
-import os, sys, string, random
+import os, sys, string, random, subprocess
      
 if __name__ == "__main__":
     len_string = 10 
     ran = ''.join(random.choices(string.ascii_uppercase + string.digits, k = len_string))
     time = sys.argv[1] if len(sys.argv) >= 2 else "4"
-    partition = sys.argv[2] if len(sys.argv) >= 3 else "LYRA1"
+    partition = sys.argv[2] if len(sys.argv) >= 3 else "LYRA2"
     ncpu = sys.argv[3] if len(sys.argv) >= 4 else "4"
     memory = sys.argv[4] if len(sys.argv) >= 5 else "48000"
     nContainer = sys.argv[5] if len(sys.argv) >= 6 else "8"
     email = sys.argv[6] if len(sys.argv) >= 7 else ""
     job_file = os.path.join(os.getcwd(), "sbatch_script_swarm" + str(ran))
-    with open(job_file) as fh:
+    with open(job_file, "w") as fh:
         fh.writelines("#!/bin/bash\n")
         fh.writelines("#SBATCH --job-name=worker-launcher\n")
         fh.writelines("#SBATCH --output=output.slurm\n")
         fh.writelines(f"#SBATCH --time={time}:00:00\n")
         fh.writelines(f"#SBATCH --mem=1\n")
+        fh.writelines(f"#SBATCH --exclusive\n")
         fh.writelines("#SBATCH --mail-type=failed,end\n")
         fh.writelines(f"#SBATCH --mail-user={email}\n")
         fh.writelines(f"#SBATCH --ntasks-per-node=1\n")
         fh.writelines(f"#SBATCH --partition {partition}\n")
-        fh.writelines("LOCALTMP=/scratch/p_esch01/$SLURM_JOB_ID")
-        fh.writelines("BACKDIR=$SLURM_SUBMIT_DIR")
-        fh.writelines("cd $BACKDIR")
-        fh.writelines("if [ ! -e $LOCALTMP ]")
-        fh.writelines("then")
-        fh.writelines("mkdir -p $LOCALTMP")
-        fh.writelines("fi")
-        fh.writelines("rsync -avu * $LOCALTMP/")
-        fh.writelines("echo $HOSTNAME:$LOCALTMP > LOCALDIR")
-        fh.writelines("cd $LOCALTMP")
-        fh.writelines(f"python3 DockerSDK.py {ncpu} {memory} {nContainer}")
-        fh.writelines("cd $BACKDIR")
-        fh.writelines('rsync -avu $LOCALTMP/* $BACKDIR/ && rm -rf $LOCALTMP && echo "$LOCALTMP deleted" >>LOCALDIR')
-        os.system(f"sbatch {job_file}")
+        fh.writelines("LOCALTMP=/scratch/p_esch01/$SLURM_JOB_ID\n")
+        fh.writelines("BACKDIR=$SLURM_SUBMIT_DIR\n")
+        fh.writelines("cd $BACKDIR\n")
+        fh.writelines("if [ ! -e $LOCALTMP ]\n")
+        fh.writelines("then\n")
+        fh.writelines("mkdir -p $LOCALTMP\n")
+        fh.writelines("fi\n")
+        fh.writelines("rsync -avu * $LOCALTMP/ \n")
+        fh.writelines("echo $HOSTNAME:$LOCALTMP > LOCALDIR \n")
+        fh.writelines("cd $LOCALTMP \n")
+        fh.writelines(f"sudo -H -u p_esch01 bash -c '/WORK/p_esch01/progs/anaconda3/bin/python3 DockerSDK.py {ncpu} {memory} {nContainer} > launcher_"+str(ran)+".out 2>error_"+str(ran)+"'\n")
+        fh.writelines("cd $BACKDIR \n")
+        fh.writelines('rsync -avu $LOCALTMP/* $BACKDIR/ && rm -rf $LOCALTMP && echo "$LOCALTMP deleted" >>LOCALDIR \n')
+    filename = os.path.join("/WORK/p_esch01/progs/restApi/src/client/",job_file)
+    os.system(f"sbatch {filename}")
